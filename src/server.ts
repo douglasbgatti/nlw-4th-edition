b59ce024d1cfeccc187a8d1adb66f74896030c00
Douglas Gatti <douglasbgatti@gmail.com>
2021-02-23 21:47:04 -0300

    Create initial ts express project

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.get("/users", async (req, res) => {
+  return res.json({ user: "test" });
+});
+
+app.listen(3333, () => {
+  console.log("[server] Server is running!");
+});
