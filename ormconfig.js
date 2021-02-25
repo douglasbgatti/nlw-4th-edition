4b3eb22efd7f80ffa82fcfc4459aa7780d30997c
Douglas Gatti <douglasbgatti@gmail.com>
2021-02-24 21:29:11 -0300

    Create user model

--- ormconfig.js
+++ ormconfig.js
@@ -1,6 +1,8 @@
 module.exports = {
   type: "sqlite",
   database: "./src/database/database.sqlite",
+  entities: ["./src/models/**.ts"],
+  logging: true,
   migrations: ["./src/database/migrations/**.ts"],
   cli: {
     migrationsDir: "./src/database/migrations",
