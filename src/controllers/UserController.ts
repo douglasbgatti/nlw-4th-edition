51f5ebc33c81ac0c183a7d963866c2be69dc0077
Douglas Gatti <douglasbgatti@gmail.com>
2021-02-28 10:52:46 -0300

    Add validations to user create

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,9 +1,26 @@
 import { Request, Response } from "express";
 import { getCustomRepository } from "typeorm";
 import { UsersRepository } from "../repositories/UsersRepository";
+import * as yup from "yup";
 
 class UserController {
+  static async validate(values: any) {
+    const schema = yup.object().shape({
+      name: yup.string().required(),
+      email: yup.string().email().required(),
+    });
+
+    await schema.validate(values, { abortEarly: false });
+  }
+
   async create(request: Request, response: Response) {
+    try {
+      await UserController.validate(request.body);
+    } catch (error) {
+      console.error(error);
+      return response.status(400).json({ error });
+    }
+
     const { name, email } = request.body;
 
     const usersRepository = getCustomRepository(UsersRepository);
